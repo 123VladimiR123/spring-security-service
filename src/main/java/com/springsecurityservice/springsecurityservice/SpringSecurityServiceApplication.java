e271627eeb4c3166475a8bc1fb7adf3089df3c6c
123VladimiR123 <888prikol888@gmail.com>
2024-04-19 22:48:46 +0300

    discovery enabled

--- src/main/java/com/springsecurityservice/springsecurityservice/SpringSecurityServiceApplication.java
+++ src/main/java/com/springsecurityservice/springsecurityservice/SpringSecurityServiceApplication.java
@@ -2,9 +2,11 @@ package com.springsecurityservice.springsecurityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringSecurityServiceApplication {
 
     public static void main(String[] args) {
