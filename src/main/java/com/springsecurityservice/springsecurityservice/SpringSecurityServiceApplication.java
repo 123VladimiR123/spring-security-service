623a69f2c9ebd7fd14b68717d07496e49a15f158
123VladimiR123 <888prikol888@gmail.com>
2024-04-20 00:26:25 +0300

    enabled jpa repositories

--- src/main/java/com/springsecurityservice/springsecurityservice/SpringSecurityServiceApplication.java
+++ src/main/java/com/springsecurityservice/springsecurityservice/SpringSecurityServiceApplication.java
@@ -3,10 +3,12 @@ package com.springsecurityservice.springsecurityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaRepositories
 public class SpringSecurityServiceApplication {
 
     public static void main(String[] args) {
