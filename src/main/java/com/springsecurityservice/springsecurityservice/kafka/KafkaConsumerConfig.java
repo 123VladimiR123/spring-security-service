b030020aa2742192727af539af9998e9211b9551
123VladimiR123 <888prikol888@gmail.com>
2024-04-21 02:09:29 +0300

    forgot ConsumerFactory

---
+++ src/main/java/com/springsecurityservice/springsecurityservice/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,28 @@
+package com.springsecurityservice.springsecurityservice.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${custom.kafka.address}")
+    private String kafkaServer;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(map);
+    }
+}
