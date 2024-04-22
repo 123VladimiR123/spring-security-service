61aacce7f1fae740b4948226e9c74593a5abe550
123VladimiR123 <888prikol888@gmail.com>
2024-04-23 01:34:26 +0300

    added consumer group id

--- src/main/java/com/springsecurityservice/springsecurityservice/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/springsecurityservice/springsecurityservice/kafka/KafkaConsumerConfig.java
@@ -23,6 +23,7 @@ public class KafkaConsumerConfig {
         map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        map.put(ConsumerConfig.GROUP_ID_CONFIG, "default");
         return new DefaultKafkaConsumerFactory<>(map);
     }
 }
