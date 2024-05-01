f729cf9a167dc67fbe5b327e5fb1be905f84d1ee
123VladimiR123 <888prikol888@gmail.com>
2024-05-01 22:16:32 +0300

    logout fix?

--- src/main/java/com/springsecurityservice/springsecurityservice/controllers/LogoutController.java
+++ src/main/java/com/springsecurityservice/springsecurityservice/controllers/LogoutController.java
@@ -3,8 +3,10 @@ package com.springsecurityservice.springsecurityservice.controllers;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/logout")
 @RestController
@@ -12,11 +14,15 @@ public class LogoutController {
 
     @Value("${jwt.cookie.name}")
     private String cookieName;
+    @Value("${LOCALHOST_GATEWAY_ADDRESS}")
+    private String prefix;
     @GetMapping
-    public void logout(HttpServletResponse response){
-        Cookie cookie = new Cookie(cookieName, "");
+    public ResponseEntity logout(HttpServletResponse response){
+        Cookie cookie = new Cookie(cookieName, null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
         response.addCookie(cookie);
+
+        return ResponseEntity.status(302).header("Location", prefix + "/login").build();
     }
 }
